f620d899d086acda9ad6731fe14f16f0f50edd89
Valter Schütz <valterschutz@protonmail.com>
2022-12-07 08:52:08 +0100

    stuff

--- main.R
+++ main.R
@@ -10,7 +10,7 @@ df$date = as.POSIXct(df$date, format="%Y%m%d")
 #df$grade = factor(df$grade)
 
 df_test = sample_n(df, 1000)
-pairs(df_test[,c("price","sqft_above","sqft_basement", "bathrooms","bedrooms","yr_built","view","condition","grade","waterfront")])
+#pairs(df_test[,c("price","sqft_above","sqft_basement", "bathrooms","bedrooms","yr_built","view","condition","grade","waterfront")])
 # 
 plot(df$price, ylim=c(0,1e5))
 m = lm(price ~ date + bedrooms + bathrooms + sqft_lot + floors + waterfront + view + condition + grade + sqft_above + sqft_basement + yr_built, data=df)
